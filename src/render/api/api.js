d2a0d5f10a8c80debb3451142405525450ef9945
EGo14T <974412906@qq.com>
2021-01-07 09:41:06 +0800

    封装接口

---
+++ src/render/api/api.js
@@ -0,0 +1,45 @@
+import * as http from "./http"
+
+/**
+ * music相关
+ */
+//检索歌曲
+export const getMusicInfo = data => http.getRequest("/music/song", false, data);
+//歌曲添加到歌单中
+export const addMusicToList = data => http.postRequest("/music/song", false, data);
+//把歌曲从歌单删除
+export const delMusic = data => http.deleteRequest("/music/song", false, data);
+//检索歌曲
+export const searchMusic = data => http.defaultGet("/song/search", false,data);
+
+/**
+ * musiclist相关
+ */
+//检索用户的歌单列表(创建&默认)
+export const getCreateMusicListInfo = data => http.getRequest("/musiclist/created", false ,data);
+//检索用户的歌单列表(收藏)
+export const getCollectMusicListInfo = data => http.getRequest("/musiclist/collected", false, data);
+//推荐歌单
+export const getDiscoverMusicListInfo = data => http.getRequest("/musiclist/discover", false, data);
+//收藏歌单操作
+export const collectMusicList = data => http.postRequest("/musiclist/collect", false, data);
+//根据歌单ID查询带状态的歌单歌曲列表
+export const getUserMusicList = data => http.getRequest("/musiclist", false, data);
+//根据歌单id返回歌单详细信息
+export const getMusicListInfo = data => http.getRequest("/musiclist/getinfo", false, data);
+//新建歌单
+export const createMusicList = data => http.postForJson("/musiclist/created", false, data);
+//删除歌单
+export const delMusiclist = data => http.deleteRequest("/musiclist", false, data);
+//更新歌单信息
+export const updateMusiclist = data => http.patchForJson("/musiclist/update", false, data);
+
+/**
+ * 评论相关
+ */
+//添加评论 相对于资源来说  每首歌曲下面的评论
+export const saveComments = data => http.postForJson("/comments/saveComments", false, data);
+//获取单条评论内容
+export const getComment = data => http.getRequest("/comments/getComment", false, data);
+//获取资源评论
+export const getAllComments = data => http.getRequest("/comments/getAllComments", false, data);
---
+++ src/render/api/http.js
@@ -0,0 +1,151 @@
+import axios from "axios"
+import { Message } from 'element-ui'
+
+
+//要请求的后台地址
+//axios.defaults.baseURL = "http://frp.ego1st.cn"
+
+//默认超时时间
+axios.defaults.timeout = 30000;
+
+//post请求的默认请求头
+axios.defaults.headers.post['Content-Type'] ='application/json';
+axios.defaults.headers['userId'] = '1';
+
+//request拦截
+axios.interceptors.request.use(config => {
+    if (config.isToken) {
+        config.headers.common['Authorization'] = 'Bearer ' + localStorage.accessToken
+    }
+    return config
+})
+
+//response拦截
+axios.interceptors.response.use(data => {
+    return data;
+}, err => {
+    if (err.response.status == 500) {
+        Message.error({message:err.response.data.message,duration:3000});
+    }
+    return Promise.reject(err);
+})
+
+/**
+    * get方法，对应get请求
+    * @param {String} url [请求的url地址]
+    * @param {Object} params [请求时携带的参数]
+*/
+export function getRequest(url, isToken ,params) {
+    return new Promise((resolve, reject) =>{
+        axios.get(getRestfulParams(url, params), {
+            isToken: isToken
+            }).then(res => {
+                resolve(res.data)
+            }).catch(err => {
+                reject(err.data)
+        })
+    });
+}
+
+export function getKvRequest(url, isToken ,params) {
+    return new Promise((resolve, reject) =>{
+        axios.get(getKvParam(url, params), {
+            isToken: isToken
+            }).then(res => {
+                resolve(res.data)
+            }).catch(err => {
+                reject(err.data)
+        })
+    });
+}
+
+export function postRequest(url, isToken ,params) {
+    return new Promise((resolve, reject) =>{
+        axios.post(getRestfulParams(url, params), {
+            isToken: isToken
+            }).then(res => {
+                resolve(res.data)
+            }).catch(err => {
+                reject(err.data)
+        })
+    });
+}
+
+export function postKvRequest(url, isToken ,params) {
+    return new Promise((resolve, reject) =>{
+        axios.post(getKvParam(url, params), {
+            isToken: isToken
+            }).then(res => {
+                resolve(res.data)
+            }).catch(err => {
+                reject(err.data)
+        })
+    });
+}
+
+export function postForJson(url, isToken ,params) {
+    return new Promise((resolve, reject) =>{
+        axios.post(url, {
+            isToken: isToken,
+            data: params
+            }).then(res => {
+                resolve(res.data)
+            }).catch(err => {
+                reject(err.data)
+        })
+    });
+}
+
+export function patchRequest(url, isToken ,params) {
+    return new Promise((resolve, reject) =>{
+        axios.patch(getRestfulParams(url, params), {
+            isToken: isToken
+            }).then(res => {
+                resolve(res.data)
+            }).catch(err => {
+                reject(err.data)
+        })
+    });
+}
+
+export function patchForJson(url, isToken ,params) {
+    return new Promise((resolve, reject) =>{
+        axios.patch(url, {
+            isToken: isToken,
+            data: params
+            }).then(res => {
+                resolve(res.data)
+            }).catch(err => {
+                reject(err.data)
+        })
+    });
+}
+
+export function deleteRequest(url, isToken ,params) {
+    return new Promise((resolve, reject) =>{
+        axios.delete(getRestfulParams(url, params), {
+            isToken: isToken
+            }).then(res => {
+                resolve(res.data)
+            }).catch(err => {
+                reject(err.data)
+        })
+    });
+}
+
+function getRestfulParams(url, params) {
+    if (params == null) return url;
+    for (const item of params) {
+        url += "/" + item;
+    }
+    return url;
+}
+
+function getKvParam(url, params){
+    let data = '';
+    for(var k in params){
+        let value = params[k] !==undefined ? params[k] : '';
+        data += `&${k}=${encodeURIComponent(value)}`
+    }
+    return data ? url + '?' + data.substring(1) : ''
+}
